d72462b3efb757e41af06555a6c7aad776160d5b
Yang Sheng Han <progamesigner@gmail.com>
2023-01-31 09:41:28 +0000

    Update setting margins

--- src/routes/Setting.svelte
+++ src/routes/Setting.svelte
@@ -11,17 +11,15 @@
 <div class="flex w-full py-4">
   <div class="basis-6/12 border-r border-blue-400 pr-2">
     <div class="flex flex-col">
-      <label class="mb-2">
-        Layout
-        <div class="flex flex-col">
-          <label class="ml-2">
-            <input type="radio" name="layout" value="Landscape" bind:group={layout} /> Landscape
-          </label>
-          <label class="ml-2">
-            <input type="radio" name="layout" value="Portrait" bind:group={layout} /> Portrait
-          </label>
-        </div>
-      </label>
+      <p class="mb-2">Layout</p>
+      <div class="flex flex-col">
+        <label class="mb-2 ml-2">
+          <input type="radio" name="layout" value="Landscape" bind:group={layout} /> Landscape
+        </label>
+        <label class="mb-2 ml-2">
+          <input type="radio" name="layout" value="Portrait" bind:group={layout} /> Portrait
+        </label>
+      </div>
       <label class="mb-2">
         <input type="checkbox" bind:checked={hidePassword} /> Hide Password
       </label>
@@ -31,27 +29,27 @@
     </div>
   </div>
   <div class="basis-6/12 border-l border-blue-400 pl-2">
-    <p>Encryption</p>
+    <p class="mb-2">Encryption</p>
     <div class="flex flex-col">
-      <label class="mb-2">
+      <label class="mb-2 ml-2">
         <input type="radio" name="encryption" value="nopass" bind:group={encryptionMode} /> None
       </label>
-      <label class="mb-2">
+      <label class="mb-2 ml-2">
         <input type="radio" name="encryption" value="WPA" bind:group={encryptionMode} />
         WPA/WPA2/WPA3
       </label>
-      <label class="mb-2">
+      <label class="mb-2 ml-2">
         <input type="radio" name="encryption" value="WPA2-EAP" bind:group={encryptionMode} />
         WPA2-EAP
       </label>
-      <label class="mb-2">
+      <label class="mb-2 ml-2">
         <input type="radio" name="encryption" value="WEP" bind:group={encryptionMode} /> WEP
       </label>
     </div>
     {#if encryptionMode === 'WPA2-EAP'}
-      <p>EAP Method</p>
+      <p class="mb-2">EAP Method</p>
       <div class="flex flex-col">
-        <label class="mb-2">
+        <label class="mb-2 ml-2">
           <input type="radio" name="eap" value="PWD" bind:group={eapMethod} /> PWD
         </label>
       </div>
