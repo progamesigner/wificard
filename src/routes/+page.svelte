219be6cee2498772ef7687dace9ffa633f31e7d6
Yang Sheng Han <progamesigner@gmail.com>
2023-02-06 09:26:41 +0000

    Support validating login information before printing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,57 @@
   import { default as WifiCard, Layout, EAPMethod, EncryptionMode } from './WifiCard.svelte'
   import { default as WifiIcon } from './WifiIcon.svelte'
 
+  let eapIdentity: string = ''
+  let eapIdentityError: string = ''
   let eapMethod: EAPMethod = EAPMethod.PWD
   let encryptionMode: EncryptionMode = EncryptionMode.WPA
   let hiddenSSID: boolean = false
   let hidePassword: boolean = false
   let layout: Layout = Layout.Landscape
+  let password: string = ''
+  let passwordError: string = ''
+  let ssid: string = ''
+  let ssidError: string = ''
 
   function print(): void {
-    window.print()
+    eapIdentityError = ''
+    passwordError = ''
+    ssidError = ''
+
+    if (ssid.length === 0) {
+      ssidError = 'Network name is required'
+    }
+    if (encryptionMode === EncryptionMode.WEP && password.length < 5) {
+      passwordError =
+        'Password must be at least 5 characters, or change the encryption mode to "None"'
+    }
+    if (encryptionMode === EncryptionMode.WPA && password.length < 8) {
+      passwordError =
+        'Password must be at least 8 characters, or change the encryption mode to "None"'
+    }
+    if (encryptionMode === EncryptionMode.WPA2EAP) {
+      if (eapIdentity.length === 0) {
+        eapIdentityError = 'Username is required'
+      }
+      if (password.length === 0) {
+        passwordError = 'Password is required'
+      }
+    }
+
+    if (ssidError.length === 0 && passwordError.length === 0 && eapIdentityError.length === 0) {
+      window.print()
+    }
   }
 </script>
 
 <svelte:head>
-  <title>Wi-Fi Card</title>
+  {#key ssid}
+    {#if ssid.length > 0}
+      <title>Wi-Fi Card - {ssid}</title>
+    {:else}
+      <title>Wi-Fi Card</title>
+    {/if}
+  {/key}
   <meta
     name="description"
     content="Print a simple card with Wi-Fi details. Tape it to fridge, keep it in wallet, or display it in store, etc."
@@ -46,7 +84,22 @@
 
 <div class="container mx-auto mb-auto max-w-xl">
   <section class="mb-2 print:block">
-    <WifiCard {eapMethod} {encryptionMode} {hiddenSSID} {hidePassword} {layout} />
+    <WifiCard
+      {eapIdentityError}
+      {eapMethod}
+      {encryptionMode}
+      {hiddenSSID}
+      {hidePassword}
+      {layout}
+      {passwordError}
+      {ssidError}
+      bind:eapIdentity
+      bind:password
+      bind:ssid
+      on:eapIdentityChanged={() => (eapIdentityError = '')}
+      on:passwordChanged={() => (passwordError = '')}
+      on:ssidChanged={() => (ssidError = '')}
+    />
   </section>
 
   <div class="mb-2 print:hidden">
