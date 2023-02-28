42134ac10ad5502d7996cf3e38b22c54a2685fc4
Yang Sheng Han <progamesigner@gmail.com>
2023-02-28 08:10:38 +0000

    Update dependencies

--- playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import type { PlaywrightTestConfig } from '@playwright/test'
-
-export default {
-  webServer: {
-    command: 'npm run build && npm run preview',
-    port: 4173,
-  },
-  testDir: 'tests',
-} satisfies PlaywrightTestConfig
